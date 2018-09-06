7fdaab8ac0ab145115d142aa6046dc6109c3e68e
gernertj12 <JackGernert12@gmail.com>
2018-09-05 18:54:55 -0700

    Added randomized card function

--- WarApp/ViewController.swift
+++ WarApp/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftRandomNumber = arc4random_uniform(13) + 2
+        print("left random number is: \(leftRandomNumber)")
+        
+        let rightRandomNumber = arc4random_uniform(13) + 2
+        print("rightRandomNumber is: \(rightRandomNumber)")
+    
+        
+        leftImageView.image = UIImage(named: "card\(leftRandomNumber)")
+        
+        rightImageView.image = UIImage(named: "card\(rightRandomNumber)")
+    }
+    
 }
 
